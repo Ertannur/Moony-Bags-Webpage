b898f57bb159c26023636cd4ea68bca597cc7030
Ertannur Atabay <191880604+Ertannur@users.noreply.github.com>
2025-02-19 20:08:01 +0300

    toggle first

---
+++ assets/JS/myFunction.js
@@ -0,0 +1,15 @@
+ function myFunction() {
+     var x = document.getElementById('myLinks');
+      x.classList.toggle('active');
+    
+    
+    
+    
+     //  if (x.style.display === "block") {
+    //    x.style.display = "none";
+    //  } else {
+    //    x.style.display = "block";
+    //  }
+   }
+
+
